d6f299421e86eeedbb9daa02b60c57bd7ff779d9
ferror <zmalcherczyk@gmail.com>
2021-11-11 18:29:44 +0100

    Install and configure Vue UI Library primevue

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,19 @@
 import { createApp } from 'vue'
 import App from '@/App.vue'
 import router from '@/router'
+import PrimeVue from 'primevue/config';
 
-createApp(App)
-    .use(router)
-    .mount('#app')
+import 'primevue/resources/themes/saga-blue/theme.css';
+import 'primevue/resources/primevue.min.css';
+import 'primeicons/primeicons.css';
+
+const app = createApp(App);
+
+import Button from 'primevue/button';
+
+app.use(router);
+app.use(PrimeVue)
+
+app.component('Button', Button)
+
+app.mount('#app')
