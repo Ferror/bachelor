2dc51007f05885e21dc51078bd2bcace5a0afdca
ferror <zmalcherczyk@gmail.com>
2021-11-23 21:25:46 +0100

    Install and configure sentry

--- backend/config/bundles.php
+++ backend/config/bundles.php
@@ -3,4 +3,5 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    Sentry\SentryBundle\SentryBundle::class => ['all' => true],
 ];
